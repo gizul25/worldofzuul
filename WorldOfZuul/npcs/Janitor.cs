db1efe75f861cdc7fa9d0f204386d44b5332a456
GosiaU <malgosia.fretka@gmail.com>
2025-10-27 14:37:16 +0100

    Added Mechanic and Janitor NPC

---
+++ WorldOfZuul/npcs/Janitor.cs
@@ -0,0 +1,11 @@
+namespace WorldOfZuul;
+
+public class Janitor : NPC {
+    public override string GetName() {
+        return "Patrick";
+    }
+
+    public override string GetJobTitle() {
+        return "Janitor";
+    }
+}
---
+++ WorldOfZuul/npcs/Mechanic.cs
@@ -0,0 +1,11 @@
+namespace WorldOfZuul;
+
+public class Mechanic : NPC {
+    public override string GetName() {
+        return "Mike";
+    }
+
+    public override string GetJobTitle() {
+        return "Mechanic";
+    }
+}
