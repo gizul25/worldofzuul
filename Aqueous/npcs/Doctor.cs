b575d9687eebf615afdedd9650ca314b1aaa1a03
alvaro <alvil25@student.sdu.dk>
2025-10-27 20:47:49 +0100

    Added 2 NPCs

---
+++ Aqueous/npcs/Doctor.cs
@@ -0,0 +1,14 @@
+namespace Aqueous;
+
+public class Doctors : NPC
+{
+    public override string GetName() {
+        return "Bruce";
+    }
+
+    public override string GetJobTitle()
+    {
+        return "Doctor";
+    }
+
+}
---
+++ Aqueous/npcs/Electrician.cs
@@ -0,0 +1,14 @@
+namespace Aqueous;
+
+public class Electrician : NPC
+{
+    public override string GetName() {
+        return "Joseph";
+    }
+
+    public override string GetJobTitle()
+    {
+        return "Electrician";
+    }
+
+}
