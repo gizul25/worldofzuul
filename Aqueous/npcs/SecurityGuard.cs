bffdb92a85fc5f620f43a28fc6b894448cd6d4f9
kyro <asavenko883@gmail.com>
2025-10-27 16:17:29 +0100

    Edited SecurityGuard.cs

--- Aqueous/npcs/SecurityGuard.cs
+++ Aqueous/npcs/SecurityGuard.cs
@@ -1,1 +1,13 @@
+namespace Aqueous;
+public class SecurityGuard : NPC
+{
+    public override string GetName()
+    {
+        return "Barry";
+    }
 
+    public override string GetJobTitle()
+    {
+        return "Security Guard";
+    }
+}
