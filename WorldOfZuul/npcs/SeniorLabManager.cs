206504406fcfe4e49a54dc8f330e0ee50dd85745
kyro <asavenko883@gmail.com>
2025-10-27 14:38:01 +0100

    Added 3 NPCs

---
+++ WorldOfZuul/npcs/SeniorLabManager.cs
@@ -0,0 +1,14 @@
+namespace WorldOfZuul;
+
+public class SeniorLabManager : NPC
+{
+    public override string GetName()
+    {
+        return "Claire";
+    }
+
+    public override string GetJobTitle()
+    {
+        return "Senior Lab Manager";
+    }
+}
---
+++ WorldOfZuul/npcs/StationBiologist.cs
@@ -0,0 +1,14 @@
+namespace WorldOfZuul;
+
+public class StationBiologist : NPC
+{
+    public override string GetName()
+    {
+        return "Helen";
+    }
+
+    public override string GetJobTitle()
+    {
+        return "Station Biologist";
+    }
+}
