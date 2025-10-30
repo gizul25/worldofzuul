c30f95e84223021b65c29aeff2d958ff916bee01
alvaro <alvil25@student.sdu.dk>
2025-10-30 14:05:53 +0100

    Added 3 ROOMS

---
+++ Aqueous/rooms/Delivery.cs
@@ -0,0 +1,11 @@
+namespace Aqueous;
+
+public class Delivery : Room {
+    public override string GetName() {
+        return "Delivery";
+    }
+
+    public override string GetDescription() {
+        return "This is the Delivery";
+    }
+}
---
+++ Aqueous/rooms/Storage.cs
@@ -0,0 +1,11 @@
+namespace Aqueous;
+
+public class Storage : Room {
+    public override string GetName() {
+        return "Storage";
+    }
+
+    public override string GetDescription() {
+        return "This is  the Storage";
+    }
+}
