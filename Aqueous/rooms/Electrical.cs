cc31c7e06389f6e232a6bd6c1ab802ab0a8bbb72
Gergely <gevar25@student.sdu.dk>
2025-10-30 13:59:06 +0100

    Added Electricals

---
+++ Aqueous/rooms/Electrical.cs
@@ -0,0 +1,11 @@
+namespace Electricals;
+
+public class Electricals : Room {
+    public override string GetName() {
+        return "Electricals";
+    }
+
+    public override string GetDescription() {
+        return "This is a Electricals";
+    }
+}
