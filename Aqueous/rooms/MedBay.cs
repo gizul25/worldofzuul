ce1abdf87094fca86172fafabb8f3f5fd49546b0
GosiaU <malgosia.fretka@gmail.com>
2025-10-30 14:00:12 +0100

    Added 3 rooms (SleepingQuarters,MedBay,MainLab)

---
+++ Aqueous/rooms/MedBay.cs
@@ -0,0 +1,11 @@
+namespace Aqueous;
+
+public class MedBay : Room {
+    public override string GetName() {
+        return "MedBay";
+    }
+
+    public override string GetDescription() {
+        return "This is the station's Med Bay";
+    }
+}
