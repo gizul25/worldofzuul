0da474760e0ffc02ffc2bd833ff4d39fccf3e978
Gergely <gevar25@student.sdu.dk>
2025-10-30 13:53:54 +0100

    Added Cooling system

---
+++ Aqueous/rooms/Coolingsystem.cs
@@ -0,0 +1,11 @@
+namespace Coolingsystem;
+
+public class Coolingsystem : Room {
+    public override string GetName() {
+        return "Cooling system";
+    }
+
+    public override string GetDescription() {
+        return "This is a Cooling system";
+    }
+}
