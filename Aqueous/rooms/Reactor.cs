94c9607d32ee30d17fb953cf0ad401eb33b588f2
Gergely <gevar25@student.sdu.dk>
2025-10-30 13:56:43 +0100

    Added Reactor

---
+++ Aqueous/rooms/Reactor.cs
@@ -0,0 +1,12 @@
+namespace Reactor;
+
+public class Reactor : Room
+{
+    public override string GetName() {
+        return "Reactor";
+    }
+
+    public override string GetDescription() {
+        return "This is Reactor";
+    }
+}
