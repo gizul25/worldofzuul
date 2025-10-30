1290f93cb3389c15b8b02765627dee43c350e74d
gizul25 <gizul25@student.sdu.dk>
2025-10-30 13:21:22 +0000

    fix name of cooling system

---
+++ Aqueous/rooms/CoolingSystem.cs
@@ -0,0 +1,11 @@
+namespace Aqueous;
+
+public class CoolingSystem : Room {
+    public override string GetName() {
+        return "Cooling system";
+    }
+
+    public override string GetDescription() {
+        return "This is a Cooling system";
+    }
+}
