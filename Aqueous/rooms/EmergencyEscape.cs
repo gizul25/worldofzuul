f322df12e82f0633b797cadb30b8289d17639535
Kyro Nelson <asavenko883@gmail.com>
2025-10-30 14:06:25 +0100

    Added Security and Emergency Exit

---
+++ Aqueous/rooms/EmergencyEscape.cs
@@ -0,0 +1,12 @@
+namespace Aqueous;
+public class EmergencyExit : Room
+{
+    public override string GetName()
+    {
+        return "Emergency Exit";
+    }
+    public override string GetDescription()
+    {
+        return "This is an Emergency Exit";
+    }
+}
---
+++ Aqueous/rooms/Security.cs
@@ -0,0 +1,12 @@
+namespace Aqueous;
+public class Security : Room
+{
+    public override string GetName()
+    {
+        return "Security";
+    }
+    public override string GetDescription()
+    {
+        return "This is a Security room";
+    }
+}
