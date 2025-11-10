da966b7b1476235ef33f77b8c26c88b7aa1015e6
GosiaU <malgosia.fretka@gmail.com>
2025-11-10 13:24:47 +0100

    Added item Leftovers

---
+++ Aqueous/items/Leftovers.cs
@@ -0,0 +1,11 @@
+namespace Aqueous;
+
+public class Leftovers : IItem {
+    public string GetName() {
+        return "Leftovers from Joey";
+    }
+
+    public string GetDescription() {
+        return "Bits and pieces of yesterday's dinner. Something a certain fluff ball would love to get its paws on.";
+    }
+}
