188d51f978938fb2a0c7dabbc21ca4a911f6c755
GosiaU <malgosia.fretka@gmail.com>
2025-11-13 14:48:00 +0100

    Added/edited more text

--- Aqueous/actions/ShieldsMaintenance.cs
+++ Aqueous/actions/ShieldsMaintenance.cs
@@ -20,7 +20,9 @@ public class ShieldMaintenance : Action {
     public override void Perform(GameState state) {
         base.Perform(state);
         Complete();
-        Console.WriteLine("Maintaining shields....");
+        Console.WriteLine("You approach the massive machine in the center of the room and access the system.");
+        Console.WriteLine("Everything seems to be in working order but you order a shields check just in case.");
+        Console.WriteLine("It comes out positive, the station is safe. At least from external dangers.");
         Console.WriteLine("For your hard work, now you get a security key!");
         state.itemManager.AddItem(new SecurityKey());
     }
