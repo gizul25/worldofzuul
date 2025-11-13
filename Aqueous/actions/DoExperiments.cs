8aaa826c54a7092e17bed57556e446ea9f78b00d
GosiaU <malgosia.fretka@gmail.com>
2025-11-13 14:20:20 +0100

    Added/edited more text

--- Aqueous/actions/DoExperiments.cs
+++ Aqueous/actions/DoExperiments.cs
@@ -35,11 +35,11 @@ public class DoExperiments : Action {
     public override void Perform(GameState state) {
         base.Perform(state);
         Console.WriteLine("You grab a few vials and a bunch of extra equipment. Your hands are stlightly shaky as you carefully mix the solutions.");
-        Console.WriteLine("Thankfully those particular chemicals won't explode if you mix them wrong and you finish the job in one piece.");
+        Console.WriteLine("Thankfully those particular chemicals won't explode if you mix them wrong. At least they shouldn't.");
 
         while (true)
         {
-            Console.Write("Mix chemicals: ");
+            Console.Write("Helen: “Oh, you're here. Good. You'll help make some medicine for Mickey. Here, take these vials and mix them in this order.” Helen points at the vials - fourth, second, first, third and fifth respectively. Mix chemicals: ");
             int[] array = new int[5];
             int[] validAnswer = { 4, 2, 1, 3, 5 };
             for (int i = 0; i < array.Length; i++)
@@ -52,13 +52,13 @@ public class DoExperiments : Action {
 
             if (array.SequenceEqual(validAnswer))
             {
-                Console.WriteLine("Good job. Maybe I should take advantage of you more often.");
+                Console.WriteLine("Helen: “Good job. Maybe I should take advantage of you more often.”");
                 Complete();
                 break;
             }
             else
             {
-                Console.WriteLine("Hm, this doesn’t seem right. Could you try again? Maybe listen to my intructions this time, why don’t you?");
+                Console.WriteLine("Helen: “Hm, this doesn’t seem right. Could you try again? Maybe listen to my intructions this time, why don’t you?”");
             }
         }
         Console.WriteLine("Added blood sample");
