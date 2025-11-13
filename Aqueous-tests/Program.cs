b2510e99121800057a79713b7c394b35269b8ce1
gizul25 <gizul25@student.sdu.dk>
2025-11-13 13:58:39 +0000

    add testing

---
+++ Aqueous-tests/Program.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Aqueous;
+
+namespace UnitTests
+{
+    public class AqueousTests
+    {
+        GameState state;
+        
+        [SetUp]
+        public void Setup()
+        {
+            state = new GameState();
+        }
+        
+        [Test]
+        public void ValidDirectionParsing()
+        {
+            Assert.AreEqual(Direction.North, DirectionHelper.DirectionFromString("north"));
+            Assert.AreEqual(Direction.South, DirectionHelper.DirectionFromString("south"));
+            Assert.AreEqual(Direction.West, DirectionHelper.DirectionFromString("west"));
+            Assert.Pass();
+        }
+
+        [Test]
+        public void MoveBetweenRooms()
+        {
+            Room room = state.CurrentRoom;
+            Room nextRoom = state.roomManager.GetRoom(typeof(Reactor));
+            state.MoveToRoom(nextRoom);
+            Room currentRoom = state.CurrentRoom;
+            Assert.AreEqual(nextRoom, currentRoom);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AddItemToInventory()
+        {
+            Assert.AreEqual(0, state.itemManager.Items.Count);
+            state.itemManager.AddItem(new SecurityKey());
+            Assert.AreEqual(1, state.itemManager.Items.Count);
+            Assert.Pass();
+        }
+    }
+}
+
