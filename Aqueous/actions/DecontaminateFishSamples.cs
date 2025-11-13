67c41461d8874670c318886434bffe2f3787869d
GosiaU <malgosia.fretka@gmail.com>
2025-11-13 15:19:54 +0100

    Added/edited more text

--- Aqueous/actions/DecontaminateFishSamples.cs
+++ Aqueous/actions/DecontaminateFishSamples.cs
@@ -23,19 +23,19 @@ public class DecontaminateFishSamples : Action {
 
     public override void Perform(GameState state) {
         base.Perform(state);
-        Console.WriteLine("Hmmm... this fish tank looks rather contaminated. You should clean it before the fish die.");
+        Console.WriteLine("These fish tanks look rather dirty and might be contaminated. You should clean them before the fish die.");
         Console.WriteLine("Type \"clean\" five times to clean up the fish tank.");
         
         int cleanCounter = 0;
         while (cleanCounter < 5) {
             string? input = Console.ReadLine();
             if (input != "clean") {
-                Console.WriteLine("This action doesn't seem to clean up the tank.");
+                Console.WriteLine("This action doesn't seem to clean up the tanks.");
                 continue;
             }
             cleanCounter++;
         }
-        Console.WriteLine("The tank is clear and the glass isn't green from algae anymore. The fish are safe for now.");
+        Console.WriteLine("The tanks are clean and the glass isn't green from algae anymore. The fish are safe for now.");
         Complete();
     }
 }
